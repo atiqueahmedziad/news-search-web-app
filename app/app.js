797f9320b0240951b5c9abed4e6eabb99f804276
atiqueahmedziad <softfilebd@gmail.com>
2020-04-06 14:46:32 +0600

    updating app.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 'use strict'
 
-var express = require('express');
-var cookieParser = require('cookie-parser')
-var http = require('http')
-var reload = require('reload');
-var app = express();
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const http = require('http')
+const reload = require('reload');
+const app = express();
 
 app.set('port', process.env.PORT || 4000 );
 app.set('view engine', 'ejs');
@@ -18,15 +18,13 @@ app.use(require('./routes/index'));
 app.use(require('./routes/search'));
 app.use(require('./routes/api'));
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
-reload(app).then(function (reloadReturned) {
-  // reloadReturned is documented in the returns API in the README
-
-  // Reload started, start web server
+reload(app).then((reloadReturned) => {
+  // reloadReturned is documented in the reload library documentation
   server.listen(app.get('port'), function () {
     console.log('Web server listening on port ' + app.get('port'))
-  })
-}).catch(function (err) {
-  console.error('Reload could not start, could not start server/sample app', err)
-})
+  });
+}).catch(error => {
+  console.error('Reload could not start, could not start server/sample app', error);
+});
